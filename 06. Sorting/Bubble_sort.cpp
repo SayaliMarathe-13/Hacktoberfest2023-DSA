420f785af74084923e10a9c3d868e40fbfdda5ab
SayaliMarathe-13 <marathesayali2003@gmail.com>
2023-10-01 10:10:00 +0530

    implemented Bubble sort algorithm

---
+++ 06. Sorting/Bubble_sort.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+
+        
+        if (!swapped) {
+            break;
+        }
+    }
+}
+
+int main() {
+    int n;
+
+    std::cout << "Enter the number of elements: ";
+    std::cin >> n;
+
+    int arr[n];
+
+    std::cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        std::cin >> arr[i];
+    }
+
+    std::cout << "Original array: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    bubbleSort(arr, n);
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
